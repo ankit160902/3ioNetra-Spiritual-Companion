2d04c9126b38c6d81571e892edc8a337f0eb2436
ankit160902 <ankit.tripathi@3ionetra.com>
2026-02-02 09:32:58 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -47,7 +47,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://3io-netra-spiritual-companion.vercel.app",
+        "https://3io-netra-spiritual-companion-vd8m.vercel.app/",
         "http://localhost:3000",
         "http://localhost:3001",
         "http://localhost:8000",
